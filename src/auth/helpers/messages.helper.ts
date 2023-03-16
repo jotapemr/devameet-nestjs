5c81462e956cb3ef01536236aa89db0d6da5f2ee
jotapemr <101481431+jotapemr@users.noreply.github.com>
2023-03-16 18:11:02 -0300

    Login

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,6 @@
+export const MessagesHelper = {
+    AUTH_LOGIN_NOT_FOUND: 'Login informado inválido',
+    AUTH_PASSWORD_NOT_FOUND: 'Senha informada inválida',
+    AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: 'Email e/ou senha não encontrados',
+
+}
