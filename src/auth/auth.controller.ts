42b0471201d83430b573430bb871bb4f052f60ad
jotapemr <101481431+jotapemr@users.noreply.github.com>
2023-03-17 16:18:08 -0300

    Register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, HttpCode, HttpStatus, Body } from "@nestjs/common";
+import { RegisterDto } from "src/user/dtos/register.dto";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dtos/login.dto";
 
@@ -6,9 +7,15 @@ import { LoginDto } from "./dtos/login.dto";
 export class AuthController{
     constructor(private readonly authService: AuthService){}
 
-    @Post('Login')
+    @Post('login')
     @HttpCode(HttpStatus.OK)
     login(@Body() dto : LoginDto){
         return this.authService.login(dto);
+    }
+
+    @Post('register')
+    @HttpCode(HttpStatus.OK)
+    register(@Body() dto : RegisterDto){
+        return this.authService.register(dto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,33 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, Logger } from "@nestjs/common";
+import { RegisterDto } from "src/user/dtos/register.dto";
+import { UserMessagesHelper } from "src/user/helpers/messages.helper";
+import { UserService } from "src/user/user.service";
 import { LoginDto } from "./dtos/login.dto";
 import { MessagesHelper } from "./helpers/messages.helper";
 
 @Injectable()
 export class AuthService{
+    private looger = new Logger(AuthService.name)
+
+    constructor(private readonly userService: UserService){
+
+    }
 
     login(dto: LoginDto){
+        this.looger.debug('login - started')
         if(dto.login !== 'teste@teste.com' || dto.password !== 'teste123'){
            throw new BadRequestException(MessagesHelper.AUTH_PASSWORD_OR_LOGIN_NOT_FOUND);          
         }
 
         return dto;
+    }
+
+    async register(dto: RegisterDto){
+        this.looger.debug('register - started')
+        if(await this.userService.existByEmail(dto.email)){
+            throw new BadRequestException(UserMessagesHelper.REGISTER_EXIST_EMAIL_ACCOUNT)
+        }
+
+        await this.userService.create(dto);
     }
 }
