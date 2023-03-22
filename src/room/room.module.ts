4add35d6f509695df5995c9ea9c1ca4bc2430131
jotapemr <101481431+jotapemr@users.noreply.github.com>
2023-03-22 18:54:34 -0300

    web socket

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -5,12 +5,13 @@ import { MeetModule } from 'src/meet/meet.module';
 import { UserModule } from 'src/user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Position, PositionSchema } from './schemas/position.schema';
+import { RoomGateway } from './room.gateway';
 
 @Module({
   imports: [
     MeetModule, UserModule, MongooseModule.forFeature([{name: Position.name, schema: PositionSchema}])
   ],
-  providers: [RoomService],
+  providers: [RoomService, RoomGateway],
   controllers: [RoomController]
 })
 export class RoomModule {}
